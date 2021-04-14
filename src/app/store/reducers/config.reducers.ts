d90bbfbedd1e0608b43670c198f269e93ef757d3
Prince Donkor <prince.donkor74@gmail.com>
2021-04-14 17:07:34 -0500

    created store

---
+++ src/app/store/reducers/config.reducers.ts
@@ -0,0 +1,19 @@
+import { state } from "@angular/animations";
+import { ConfigActions, EConfigActions } from "../actions/config.actions";
+import { IConfigState, initialConfigState } from "../state/config.state";
+
+export const configReducers = (
+    state = initialConfigState,
+    action: ConfigActions
+): IConfigState => {
+    switch (action.type) {
+        case EConfigActions.GetConfigSuccess:{
+            return {
+                ...state,
+                config: action.payload
+            }
+        }
+        default:
+            return state
+    }
+}
